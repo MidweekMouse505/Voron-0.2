acf0877c16b0c9e9ccc882723c1b8e8ed29a3374
biqu <biqu@V0.2>
2024-05-24 18:07:25 +0000

    Autocommit from 2024-05-24 18:07:25
    Klipper version: v0.12.0-204-g4a92727e
    
    Moonraker version: v0.8.0-359-g73df63d
    
    Mainsail version: v2.11.2

--- autocommit.sh
+++ autocommit.sh
@@ -34,6 +34,10 @@ mainsail_folder=~/mainsail
 ### By default that is 'main'
 branch=main
 
+### Set this to true if you want ONLY the history table to be dumped
+### from data.mdb.
+history_only=false
+
 #####################################################################
 #####################################################################
 
@@ -43,16 +47,12 @@ branch=main
 #####################################################################
 grab_version(){
   if [ ! -z "$klipper_folder" ]; then
-    cd "$klipper_folder"
-    klipper_commit=$(git rev-parse --short=7 HEAD)
-    m1="Klipper on commit: $klipper_commit"
-    cd ..
+    klipper_commit=$(git -C $klipper_folder describe --always --tags --long | awk '{gsub(/^ +| +$/,"")} {print $0}')
+    m1="Klipper version: $klipper_commit"
   fi
   if [ ! -z "$moonraker_folder" ]; then
-    cd "$moonraker_folder"
-    moonraker_commit=$(git rev-parse --short=7 HEAD)
-    m2="Moonraker on commit: $moonraker_commit"
-    cd ..
+    moonraker_commit=$(git -C $moonraker_folder describe --always --tags --long | awk '{gsub(/^ +| +$/,"")} {print $0}')
+    m2="Moonraker version: $moonraker_commit"
   fi
   if [ ! -z "$mainsail_folder" ]; then
     mainsail_ver=$(head -n 1 $mainsail_folder/.version)
@@ -63,6 +63,24 @@ grab_version(){
     m4="Fluidd version: $fluidd_ver"
   fi
 }
+
+# Here we dump stats database to text format for backup, IF the right software is found
+# To RESTORE the database, use the following command:
+# mdb_load -f ~/printer_data/config/data.mdb.backup -s -T ~/printer_data/database/
+
+if command -v /usr/bin/mdb_dump &> /dev/null
+then
+    if $history_only
+    then
+        echo "mdb_dump found! Exporting history table from data.mdb to ~/printer_data/config/data.mdb.backup"
+        mdb_dump -s history -n ~/printer_data/database/data.mdb -f ~/printer_data/config/data.mdb.backup
+    else
+        echo "mdb_dump found! Exporting ALL tables data.mdb to ~/printer_data/config/data.mdb.backup"
+        mdb_dump -a -n ~/printer_data/database/data.mdb -f ~/printer_data/config/data.mdb.backup
+    fi
+else
+    echo "mdb_dump not found! Consider installing it via 'sudo apt install lmdb-utils' if you want to back up your statistics database!"
+fi
 
 # To fully automate this and not have to deal with auth issues, generate a legacy token on Github
 # then update the command below to use the token. Run the command in your base directory and it will
